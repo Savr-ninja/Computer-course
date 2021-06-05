bd01630990e3e3edfb376c6ff43ed2ac8580c1db
Savr Ledzhenov <sledzhenov@gmail.com>
2021-06-05 17:18:38 +0300

    added script for mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
+document.getElementById("trigger").onclick = function() {
+  open()
+};
+function open() {
+	document.getElementById("menu-body").classList.toggle("show");
+	document.getElementById("burger").classList.toggle("invisible");
+  };
 
+  document.getElementById("click").onclick = function() {
+	close()
+  };
+  
+  function close() {
+	document.getElementById("menu-body").classList.remove("show");
+	document.getElementById("burger").classList.remove("invisible");
+	document.getElementByCLass("menu__close").classList.toggle("invisible");
+  }
